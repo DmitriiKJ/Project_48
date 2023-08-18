162c64402d40c3f180dfba7173ed1d7dbee82628
kurbatovdima <kurbatovdima2006@gmail.com>
2023-08-18 19:42:26 +0200

    Staff.cpp

--- Project48/Staff.cpp
+++ Project48/Staff.cpp
@@ -1,1 +1,24 @@
 #include "Staff.h"
+
+Staff::Staff()
+{
+    premium = 0;
+}
+
+Staff::Staff(string n, string p, float s, float pr): Employee(n, p, s)
+{
+    premium = pr;
+}
+
+void Staff::show() const
+{
+    cout << name << endl;
+    cout << position << endl;
+    cout << salary << endl;
+    cout << premium << endl;
+}
+
+float Staff::calcSalary() const
+{
+    return salary + premium;
+}
