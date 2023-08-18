da4b3b006d5ae8d6f9df9db9d00be49c575ad418
kurbatovdima <kurbatovdima2006@gmail.com>
2023-08-18 17:25:31 +0200

    Employee.cpp

--- Project48/Employee.cpp
+++ Project48/Employee.cpp
@@ -1,1 +1,27 @@
 #include "Employee.h"
+
+Employee::Employee()
+{
+    name = "NoName";
+    position = "Empty";
+    salary = 0;
+}
+
+Employee::Employee(string n, string p, float s)
+{
+    name = n;
+    position = p;
+    salary = s;
+}
+
+void Employee::show() const
+{
+    cout << name << endl;
+    cout << position << endl;
+    cout << salary << endl;
+}
+
+float Employee::calcSalary() const
+{
+    return salary;
+}
